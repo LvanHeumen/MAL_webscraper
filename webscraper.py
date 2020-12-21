a8528c34c630469f58f0a1cab98057e064c9d867
Luuk van Heumen <luukvheumen@gmail.com>
2020-12-21 15:24:48 +0100

    Initial Commit

---
+++ webscraper.py
@@ -0,0 +1,20 @@
+#TODO: Try pulling from mobile, saves having to send x additional requests.
+
+# Importing required libraries
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://myanimelist.net/people/5142/Yui_Ishikawa'
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content,"html.parser")
+
+charlist = {}
+
+for character in content.findAll('h3', attrs={"class":"h3_character_name"}):
+    charname = character.text
+    charlink = character.find('a').get('href')
+    if charname not in charlist.keys():
+        charlist[charname] = charlink
+
+print(charlist)
+print(f'{len(charlist)} Characters found')
