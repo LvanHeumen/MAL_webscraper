d5ea0c3f6d2b8c0584470217204a4a71237cbb45
Luuk van Heumen <luukvheumen@gmail.com>
2020-12-22 23:46:48 +0100

    Cleanup, pull fav number as int

--- webscraper.py
+++ webscraper.py
@@ -9,22 +9,19 @@ headers_mobile = {'User-Agent':'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/
 response = requests.get(url, headers = headers_mobile, timeout=5)
 content = BeautifulSoup(response.content,"html.parser")
 
+print(content.find('h1', attrs={'class':'title fs18 lh18'}).text)
+
+charList = {}
+
 for element in content.findAll('div', attrs={'class':'va-slider-item js-ajax-loading'}):
     charName = element.h3.text
     charFav = element.span.text
+    if charFav[-1] == 's':
+        charFav = charFav[:-5]
+    else:
+        charFav = charFav[:-4]
 
-    print(f'Character name: {charName}, Character favourites: {charFav}')
+    if charName not in charList.keys():charList[charName] = int(charFav)
 
-
-'''
-charlist = {}
-# This uses the desktop site, it's easier to index for the mobile site.
-for character in content.findAll('h3', attrs={"class":"h3_character_name"}):
-    charname = character.text
-    charlink = character.find('a').get('href')
-    if charname not in charlist.keys():
-        charlist[charname] = charlink
-
-print(charlist)
-print(f'{len(charlist)} Characters found')
-'''
+print(charList)
+print(f'{len(charList)} Characters found')
