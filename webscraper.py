f25143713ab79d6c52de2262c6099e55cc859f2e
Luuk van Heumen <luukvheumen@gmail.com>
2020-12-22 16:01:19 +0100

    Call to mobile site

--- webscraper.py
+++ webscraper.py
@@ -5,11 +5,20 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://myanimelist.net/people/8/Rie_Kugimiya'
-response = requests.get(url, timeout=5)
+headers_mobile = {'User-Agent':'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36'}
+response = requests.get(url, headers = headers_mobile, timeout=5)
 content = BeautifulSoup(response.content,"html.parser")
 
-charlist = {}
+for element in content.findAll('div', attrs={'class':'va-slider-item js-ajax-loading'}):
+    charName = element.h3.text
+    charFav = element.span.text
 
+    print(f'Character name: {charName}, Character favourites: {charFav}')
+
+
+'''
+charlist = {}
+# This uses the desktop site, it's easier to index for the mobile site.
 for character in content.findAll('h3', attrs={"class":"h3_character_name"}):
     charname = character.text
     charlink = character.find('a').get('href')
@@ -18,3 +27,4 @@ for character in content.findAll('h3', attrs={"class":"h3_character_name"}):
 
 print(charlist)
 print(f'{len(charlist)} Characters found')
+'''
